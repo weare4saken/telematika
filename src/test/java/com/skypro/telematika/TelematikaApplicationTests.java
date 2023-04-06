3ef275b1f3a1f09c82e3e532b0dbbaa883bb661f
weare4saken <kondrashka91@gmail.com>
2023-04-06 11:07:37 +0700

    Initial commit

---
+++ src/test/java/com/skypro/telematika/TelematikaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.telematika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelematikaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
